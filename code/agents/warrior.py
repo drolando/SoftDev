31cb13c613aa95ca92626e2578e61fc85854e483
danielerudy90 <rudyrolly@gmail.com>
2014-01-02 20:50:10 +0100

    Warrior get the sword from the crates

--- code/agents/warrior.py
+++ code/agents/warrior.py
@@ -7,6 +7,7 @@ from fife.extensions.fife_settings import Setting
 #TDS = Setting(app_name="rio_de_hola")
 
 _STATE_NONE, _STATE_IDLE, _STATE_RUN, _STATE_FOLLOW = 0, 1, 2, 3
+_MODE_LIGHT, _MODE_ARMED = xrange(2)
 
 class Warrior(Agent):
     def __init__(self, settings, model, agentName, layer, uniqInMap=True):
@@ -15,6 +16,7 @@ class Warrior(Agent):
         self.waypoints = ((67, 80), (75, 44))
         self.waypoint_counter = 0
         self.isActive = False
+        self._mode = _MODE_LIGHT
         
         self.SPEED = 3 * float(self.settings.get("rio", "TestAgentSpeed"))
 
@@ -39,9 +41,26 @@ class Warrior(Agent):
 
     def follow_hero(self):
         self.state = _STATE_FOLLOW
-        self.agent.follow('run', self.game.agentManager.getActiveInstance(), self.SPEED)
+        if self._mode == _MODE_LIGHT:
+            self.agent.follow('run_light', self.game.agentManager.getActiveInstance(), self.SPEED)
+        else:
+            self.agent.follow('run_armed', self.game.agentManager.getActiveInstance(), self.SPEED)
+        
 
     def run(self, location):
         self.state = _STATE_RUN
-        self.agent.move('run', location, self.SPEED)
+        if self._mode == _MODE_LIGHT:
+            self.agent.move('run_light', location, self.SPEED)
+        else:
+            self.agent.move('run_armed', location, self.SPEED)
 
+    def idle(self):
+        self.state = _STATE_IDLE
+        if self._mode == _MODE_LIGHT:
+            self.agent.actOnce('stand_light')
+        else:
+            self.agent.actOnce('stand_armed')
+
+    def gotSword(self):
+        self._mode = _MODE_ARMED
+        self.idle()
--- code/game.py
+++ code/game.py
@@ -9,7 +9,7 @@ from dialog import Dialog
 TDS = Setting(app_name="rio_de_hola")
 STATE_START, STATE_PLAY, STATE_PAUSE, STATE_END = xrange(4)
 EV_START, EV_ACTION_FINISHED, EV_HIT, EV_BEE_ARRIVED = xrange(4)
-STATE_WARRIOR1, STATE_WARRIOR2, STATE_BEE1 = xrange(3)
+STATE_WARRIOR1, STATE_WARRIOR2, STATE_BEE1, STATE_SWORD = xrange(4)
 
 
 class Game():
@@ -52,7 +52,7 @@ class Game():
             if args[0] == "warrior":
                 pass
         elif ev == EV_BEE_ARRIVED:
-            if self.agentManager.beesAtHome:
+            if self.agentManager.beesAtHome():
                 self._secState = STATE_BEE1
         else:
             print "Event not recognized: {}".format(ev)
@@ -96,7 +96,7 @@ class Game():
                 elif self._secState == STATE_BEE1:
                     instance.say(beekeeperTexts[1], 5000)
                 else:
-                    instance.say("Hey!", 3000)
+                    instance.say(beekeeperTexts[2], 3000)
             if instance.getObject().getId() == 'girl':
                 girlTexts = TDS.get("rio", "girlTexts")
                 instance.say(random.choice(girlTexts), 5000)
@@ -113,7 +113,6 @@ class Game():
                         self.agentManager.warrior.say(warriorTexts[1])
                 else:
                     self.agentManager.warrior.say(warriorTexts[2])
-                #self.agentManager.warrior.follow_hero()
 
     def warr1(self, *args):
         warriorTexts = TDS.get("rio", "warriorTexts")
@@ -145,6 +144,8 @@ class Game():
     def onOpenButtonPress(self):
         if self._state == STATE_PLAY:
             self.dialog.hide_instancemenu()
+            self._secState = STATE_SWORD
+            self.agentManager.warrior.gotSword()
 
     def onFacePressed(self, face_button):
         if self._state == STATE_PLAY:
@@ -163,8 +164,8 @@ class Game():
             if self.instance_to_agent.has_key(instance.getFifeId()):
                 buttons.append('talkButton')
                 buttons.append('kickButton')
-            if instance.getId() == "sword_crate":
-                buttons.append("openButton")
+        if instance.getId() == "sword_crate" and self._secState == STATE_BEE1:
+            buttons.append("openButton")
         self.dialog.show_instancemenu(clickpoint, instance, buttons)
 
     def load(self, map):
