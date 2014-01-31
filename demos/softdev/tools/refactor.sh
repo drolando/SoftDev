e5026fab024c0d71193122f671792bb2ee9b4656
danielerudy90 <rudyrolly@gmail.com>
2014-01-31 11:24:07 +0100

    fixed refactor.sh

--- demos/softdev/tools/refactor.sh
+++ demos/softdev/tools/refactor.sh
@@ -2,9 +2,9 @@
 # that contains the application source code. 
 # The script is invoked passing two parameters: the current name of 
 # the directory and the new name.
-# E.g., ./tools/refactor.sh code sources
+# E.g., ./tools/refactor.sh code.sources
 
-# After the previous execution the "code" directory needs to be renamed
+# After the previous execution the "code. directory needs to be renamed
 # as "sources". Obviously, several "from" statements in the python source
 # files need to be adjusted as well to reflect the change.
 # To test if everything is ok, execute the previous command and then try
@@ -27,9 +27,10 @@ fi
 
 echo "Renaming $OLDNAME to $NEWNAME"
 
-for file in $(grep -rl "from $OLDNAME" $DIRPATH/*)
+for file in $(egrep -rl "(from $OLDNAME)|( $OLDNAME)|(import $OLDNAME)" $DIRPATH/*)
 do
-	sed -i "s/from $OLDNAME/from $NEWNAME/g" $file
+	sed -i "s/$OLDNAME/$NEWNAME/g" $file
 done
+
 mv $1 $DIRPATH/$NEWNAME
 
